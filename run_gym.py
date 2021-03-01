4abe3744f74e837c35a4caa67eb8a63a866ec845
Laurent Dillard <laurent.dillard@gmail.com>
2021-03-01 15:07:32 +0900

    Add updated run script

---
+++ run_gym.py
@@ -0,0 +1,69 @@
+import argparse
+import json
+import logging
+import gym
+import numpy as np
+import lib.gym_mol
+
+# For old version of tensorflow and rdkit
+# if you don't use tensorflow and kgcn, please comment out this line
+import tensorflow as tf
+
+
+from rdkit import RDLogger
+
+from lib.calculators import CalculatorFactory
+from lib.config import Config
+from lib.data_providers import MoleculeLoader
+from lib.filters import FilterFactory
+from lib.agents import MonteCarloTreeSearchAgent, RandomAgent
+
+RDLogger.logger().setLevel(RDLogger.CRITICAL)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', type=str)
+args = parser.parse_args()
+
+config = Config.load(args.config)
+
+if config.seed is not None:
+    np.random.seed(config.seed)
+
+logging.basicConfig(format="%(message)s", level=config.logging)
+molecule_loader = MoleculeLoader(file_path=config.dataset, threshold=config.threshold)
+reward_calculator = CalculatorFactory.create(config.reward_calculator, config.reward_weights, config)
+filters = [FilterFactory.create(filter_) for filter_ in config.filters]
+
+env = gym.make("molecule-v0")
+env.initialize(reward_calculator, config.draw)
+
+if config.agent == "MonteCarloTreeSearch":
+    agent = MonteCarloTreeSearchAgent(
+        filters=filters,
+        minimum_depth=config.minimum_output_depth,
+        output_type=config.output_type,
+        breath_to_depth_ratio=config.breath_to_depth_ratio,
+    )
+elif config.agent == "Random":
+    agent = RandomAgent()
+else:
+    raise ValueError(f"Agent: {config.agent} not implemented. Choose from 'MonteCarloTreeSearch', 'Random'")
+
+for compound in molecule_loader.fetch(molecules_to_process=config.generate):
+    env.set_compound(compound)
+    env.reset()
+    agent.reset(compound)
+    reward = 0
+    done = False
+    for k in range(config.monte_carlo_iterations):
+        compound, action = agent.act(compound, reward)
+        compound, reward, done, info = env.step(compound, action)
+        if done:
+            break
+
+    output = agent.get_output(compound, reward)
+    if output is None:
+        continue
+    print(json.dumps(output, indent=4))
+    for molecule in output:
+        env.render(molecule["smiles"])
